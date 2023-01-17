0d889538a0d97f5440d5754b419b94f66ad741d7
Alp Doğan <alp48832@gmail.com>
2023-01-17 15:01:37 +0300

    Adding Files

---
+++ BtcClipperDetector/BtcClipperDetector/Clipboard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtcClipperDetector
+{
+    internal class Clipboard
+    {
+        [DllImport("User32")]
+        internal static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("User32")]
+        internal static extern bool CloseClipboard();
+
+        [DllImport("User32")]
+        internal static extern bool EmptyClipboard();
+
+        [DllImport("User32")]
+        internal static extern bool IsClipboardFormatAvailable(int format);
+
+        [DllImport("User32")]
+        internal static extern IntPtr GetClipboardData(int uFormat);
+
+        [DllImport("User32", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr SetClipboardData(int uFormat, IntPtr hMem);
+        public static void SetText(string text)
+        {
+            if (!OpenClipboard(IntPtr.Zero))
+            {
+                SetText(text);
+                return;
+            }
+            EmptyClipboard();
+            SetClipboardData(13, Marshal.StringToHGlobalUni(text));
+            CloseClipboard();
+        }
+        public static string GetText()
+        {
+            string value = string.Empty;
+            OpenClipboard(IntPtr.Zero);
+            if (IsClipboardFormatAvailable(13))
+            {
+                IntPtr ptr = Clipboard.GetClipboardData(13);
+                if (ptr != IntPtr.Zero)
+                {
+                    value = Marshal.PtrToStringUni(ptr);
+                }
+            }
+            CloseClipboard();
+            return value;
+        }
+    }
+}
---
+++ BtcClipperDetector/BtcClipperDetector/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BtcClipperDetector
+{
+    internal class Program
+    {
+        static string test_address = "3FZbgi29cpjq2GjdwV8eyHuJJnkLtktZc5";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your clipboard will clean if do you want continue press any key");
+            Console.ReadKey();
+            Warning();
+            Console.WriteLine("Scanning...\n");
+            Detect();
+            Console.ReadKey();
+        }
+        static void Warning()
+        {
+            Console.WriteLine("Scanner starting please wait 5 second and dont copy nothing!!");
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+        static void Detect()
+        {
+            Clipboard.SetText(test_address);
+            Thread.Sleep(5000);
+            Match regex = Regex.Match(Clipboard.GetText(), "^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$");
+            if (regex.Success && regex.Value != test_address)
+            {
+                Console.WriteLine("Detected!");
+            }
+            else
+            {
+                Console.WriteLine("Not Detected");
+            }                    
+        }
+
+    }
+}
